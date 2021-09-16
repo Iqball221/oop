d95b5f10f8141418fa3bcef242ae1bd53f9a596a
iqball221 <iqbalgaming21w@gmail.com>
2021-09-16 16:26:59 +0700

    Mata kuliah

---
+++ lat6.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class mahasiswa dan mata kuliah</title>
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <legend>inheritance</legend>
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td><input type="text" name=""></td>
+                    <table>
+                <tr>
+                    <td>Nim</td>
+                    <td>:</td>
+                    <td><input type="text" name=""></td>
+                </tr>
+                <table>
+                <tr>
+                    <td>Nilai</td>
+                    <td>:</td>
+                    <td><input type="number" name="bil1"></td>
+                </tr>
+                <table>
+                <tr>
+                    <td>Nama dosen</td>
+                    <td>:</td>
+                    <td><input type="text" name="bil1"></td>
+                </tr>
+                <table>
+                <tr>
+                    <td>Mata kuliah</td>
+                    <td>:</td>
+                    <td><input type="text" name="bil1"></td>
+                </tr>
+                </tr>
+        </fieldset>
+    </form>
+</body>
+</html>
